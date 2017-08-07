5c6b0bd632d2dbb415aa9bf4ff0151c41656c7c0
Leandro Santos <leandro.santos@atsnet.com.br>
2017-08-07 11:38:22 -0300

    Add Lab02.

---
+++ Lab02/AndroidApp/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidApp
+{
+    [Activity(Label = "AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            var helper = new SharedProject.MySharedCode();
+            new AlertDialog.Builder(this).SetMessage(helper.GetFilePath("demo.dat")).Show();
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
---
+++ Lab02/SharedProject/MySharedCode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SharedProject
+{
+    class MySharedCode
+    {
+        public string GetFilePath(string fileName)
+        {
+#if WINDOWS_UWP
+            return Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, fileName);
+#elif __ANDROID__
+            string libraryPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            return Path.Combine(libraryPath, fileName);
+#elif WINDOWS_WPF
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), fileName);
+#endif
+        }
+    }
+}
